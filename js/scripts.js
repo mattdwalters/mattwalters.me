afdf9fd2bd19875d359188371dfc8c27c8e2f8a2
mattdwalters <matthew.walters64@gmail.com>
2018-07-11 01:30:35 -0400

    Added smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,38 @@
 $(document).ready(function() {
-    $("#projects-tabs > a > div").click(function(){
+
+    makeActive(".dev-tab", "#development-select");
+    makeActive(".design-tab", "#design-select");
+
+    $(".design").hide()
+    $(".dev-tab").click(function() {
+    	$(".design").fadeOut();
+    	$(".design").hide();
+    	$(".dev").fadeIn();
+    });
+    $(".design-tab").click(function() {
+    	$(".dev").fadeOut();
+    	$(".dev").hide();
+    	$(".design").fadeIn();
+    });
+
+    smoothScroll(".to-about", "#begin-about");
+    smoothScroll(".to-projects", "#begin-projects");
+    smoothScroll(".to-contact", "#begin-contact");
+});
+
+function smoothScroll(trigger, endScroll) {
+	$(trigger).click(function() {
+		$('html, body').animate({
+			scrollTop: $(endScroll).offset().top
+		}, 500);
+	});
+}
+
+function makeActive(trigger, activate) {
+	$(trigger).click(function(){
         $("#projects-tabs > a > div").each(function(){
             $(this).removeClass("active");
         });
-        $(this).addClass("active"); 
+        $(activate).addClass("active"); 
     });
-
-    $(".design").hide()
-    $("#dev-tab").click(function() {
-    	$(".design").fadeOut()
-    	$(".dev").fadeIn();
-    });
-    $("#design-tab").click(function() {
-    	$(".dev").fadeOut()
-    	$(".design").fadeIn()
-    });
-});
+}
